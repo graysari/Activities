12cd5f3d11b47719d9a7f023fc3fc606d7328079
graysari <165754890+graysari@users.noreply.github.com>
2024-04-25 15:43:02 +0000

    a

--- ScoreToGradeFToA.java
+++ ScoreToGradeFToA.java
@@ -1,18 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- */
+
 import java.util.*;
 public class ScoreToGradeFToA {
-
-    /**
-     * @param args the command line arguments
-     */
-   
     public static void main(String[] args) {
         
         int score;
