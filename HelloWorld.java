d4cbc7203d5c87deef6da699dbcaa0b38b6f69fb
graysari <165754890+graysari@users.noreply.github.com>
2024-05-14 02:01:57 +0000

    1

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class helloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
