afee38dae787f8caef6b9da9231429c7b3baf12c
graysari <165754890+graysari@users.noreply.github.com>
2024-05-14 02:47:53 +0000

    1

---
+++ task122/MyDate.java
@@ -0,0 +1,10 @@
+package task122;
+public class MyDate {
+    public static void main(String[] args) {
+        DateTask date1 = new DateTask(1,1,1978);
+        DateTask date2 = new DateTask(9,21,1984);
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+       // display leapyears
+    }
+}
